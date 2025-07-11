3af9fe97a2195495ff63c86282a7ef2ff76f4535
22ALR047 <madhumithas.22aim@kongu.edu>
2025-07-11 22:40:21 +0530

    initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,23 @@ import './App.css'
 import Card from './Components/Card'
 import Header from './Components/Header'
 import Footer from './Components/Footer'
-import Search from './Components/search'
+import Search from './Components/Search'
 
 function App() {
+  const [searchValue, setSearchValue] = useState('')
+
+  const handleSearchChange = (e) => {
+    setSearchValue(e.target.value)
+  }
+
   return (
     <div className="min-h-screen bg-gray-900 flex flex-col">
       <Header />
-      <Search />
+      <Search 
+        value={searchValue}
+        onChange={handleSearchChange}
+        placeholder="Search meals..."
+      />
       <main className="flex flex-col items-start justify-start flex-1 p-0">
         <Card />
       </main>
